5ecc31be6f6f5a88fe84e7aa7553b901b10d3d6f
garciajosd_ghco <garciajosd@globalhitss.com>
2024-10-17 05:45:54 -0500

    refactoring v1

--- posts/src/session.py
+++ posts/src/session.py
@@ -9,10 +9,10 @@ class SessionConfig():
 
     def url(self):
         db_user = 'postgres'
-        db_pass = 'postgres'
-        db_host = 'localhost'
+        db_pass = 'I4]E1)D2hZFo2iTA'
+        db_host = '35.223.63.199'
         db_port = 5432
-        db_name = 'db_posts'
+        db_name = 'db-posts'
         return f'postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}'
 
 

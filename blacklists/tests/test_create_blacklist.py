56f02c76755dc3d37f5625f4d7b907ec5e54c6dd
garciajosd_ghco <garciajosd@globalhitss.com>
2024-11-01 16:23:52 -0500

    add unittest

---
+++ blacklists/tests/test_create_blacklist.py
@@ -0,0 +1,20 @@
+import blacklists.tests.funtions as f
+import uuid
+
+class TestCreateBlacklist():
+
+    def create_blacklist(self):
+        str_uuid = str(uuid.uuid4())
+
+        data = {
+            'id': str_uuid,
+            'email': 'email_test_' + str_uuid + '@gmail.com',
+            'app_uuid': str_uuid,
+            'blocked_reason': 'blocked_reason_test',
+            'ip': '127.0.0.1',
+            'createdAt': '2024-10-29 00:00:00',
+        }
+
+        blacklist = f.save_line_file('blacklists/tests/blacklist.csv', data)
+
+        return blacklist
